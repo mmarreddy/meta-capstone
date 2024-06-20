75d52c477ab2268167876fa693d6dbf3fede0051
Mounika Marreddy <mouni@Mounika.local>
2024-06-20 13:39:09 +0530

    bookingWithAPI

--- my-website/src/components/BookingForm.js
+++ my-website/src/components/BookingForm.js
@@ -10,37 +10,49 @@ function BookingForm({availableTimes, dispatch}) {
         const [guests, setGuests] = useState('');
         const [seating, setSeating] = useState('Patio');
         const [occasion, setOccasion] = useState('None');
-       
+        const [name, setName] = useState('');
+        const [email, setEmail] = useState('');
+        const [phone, setPhone] = useState('');
+        const handleSubmit  = (e) => {
+         e.preventDefault();
+        }
 
         return (
-        <form className="" style={{display: "grid", maxWidth: "200px", gap: "20px", justifyContent:"center"}}>
+        <form className="" onSubmit={handleSubmit} style={{display: "grid", maxWidth: "200px", marginLeft:"40px", gap: "20px", justifyContent:"center"}}>
          <h1>Book A Reservation with Us!</h1>
-        <label htmlFor="res-date">Choose date</label>
+        <label htmlFor="res-date">Choose date:</label>
         <input type="date" id="res-date" value={date} onChange={(e) => {
-         setDate(e.target.value); 
-         dispatch({type:"updateTimes"})
+         let selectedDate =  e.target.value
+         setDate(selectedDate); 
+         dispatch({type:"updateTimes", payload: selectedDate})
         }} required/>
-        <label htmlFor="res-time">Choose time</label>
+        <label htmlFor="res-time">Choose time:</label>
         <select id="res-time" value={time} onChange={(e) => setTime(e.target.value)}required>
          {availableTimes.map((times, index) => 
          (<option key={index} value={times}>{times}</option>))
          } 
         </select>
-        <label htmlFor="guests">Number of guests</label>
+        <label htmlFor="guests">Number of guests:</label>
         <input type="number" placeholder="1" min="1" max="10" id="guests" value={guests} onChange={(e) => setGuests(e.target.value)}required/>
-        <label htmlFor="seating">Seating Preference</label>
+        {/* <label htmlFor="seating">Seating Preference</label>
         <select id="seating" value={seating} onChange={(e)=> setSeating(e.target.value)}>
                 <option value="Patio">Patio</option>
                 <option value="Bakery">Bakery</option>
                 <option value="Indoor">Indoor</option>
-            </select>
-        <label htmlFor="occasion">Occasion</label>
+            </select> */}
+        <label htmlFor="name">Name:</label>
+        <input type="textbox" placeholder='Name' id="name" value={name} onChange={(e) => setName(e.target.value)} required/>
+        <label htmlFor="email">Email:</label>
+        <input type="email" placeholder='email ID' id="email" value={email} onChange={(e) => setEmail(e.target.value)} required/>
+        <label htmlFor="phone">Phone Number:</label>
+        <input type="tel" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" placeholder='123-456-7890' id="phone" value={phone} onChange={(e) => setPhone(e.target.value)} required/>
+        <label htmlFor="occasion">Occasion:</label>
         <select id="occasion" value={occasion} onChange={(e)=> setOccasion(e.target.value)}>
            <option>Birthday</option>
            <option>Anniversary</option>
            <option>None</option>
         </select>
-        <Button type="submit">Next</Button>
+        <Button type="submit">Book Reservation</Button>
      </form>
     );
   }
--- my-website/src/components/Reservation.js
+++ my-website/src/components/Reservation.js
@@ -1,37 +1,57 @@
-import React, { useReducer} from 'react'
+import React, {useEffect, useReducer} from 'react'
 import BookingForm from './BookingForm';
 
-function updateTimes(availableTimes, action, ) {
-  switch (action.type) {
-    case "updateTimes":
-        // You can update the state here based on the action
-        // For now, let's just return the initial times array
-        return availableTimes;
-    default:
-        return availableTimes;
-  }
+
+function updateTimes(availableTimes, action, date) {
+ switch (action.type) {
+   case "updateTimes":
+     const check = fetchAPI(date);
+     if(check.length > 0) {
+       availableTimes = [...check] }
+     else {
+       availableTimes = [ '5:40 PM',
+       '6:45 PM',
+       '7:00 PM',
+       '8:30 PM',
+      '8:45PM'];
+     }
+     return availableTimes;
+   default:
+       return availableTimes;
+ }
 }
 
-const initializeTimes = (data) => {
 
-}
+const initializeTimes = () => [
+ '5:30 PM',
+ '6:30 PM',
+ '7:30 PM',
+ '8:30 PM',
+]
 
-const fetchData = () => {
-    fetch(`https://raw.githubusercontent.com/courseraap/capstone/main/api.js`) 
-      .then((response) => response.json()) 
-      .then((data) => initializeTimes(data)) 
-      .catch((error) => console.log(error)); 
-  };  
 
-  useEffect(() => { 
-    fetchData(); 
-  }, []); 
+const fetchAPI = async (date) => {
+   const response = await fetch(`https://raw.githubusercontent.com/courseraap/capstone/main/api.js`)
+     const data = await response.json()
+     console.log(data)
+     return data;
+ }; 
+
+
+
 
 function Reservation() {
-  const [availableTimes, dispatch] = useReducer(updateTimes, initializeTimes())
+ const [availableTimes, dispatch] = useReducer(updateTimes,initializeTimes());
 
-    return (
-      <BookingForm availableTimes={availableTimes} dispatch={dispatch} />
-    );
-  }
-  export default Reservation;
+
+ useEffect(() => {
+   fetchAPI();
+ }, []);
+
+
+   return (
+     <BookingForm availableTimes={availableTimes} dispatch={dispatch} />
+   );
+ }
+ export default Reservation;
+
