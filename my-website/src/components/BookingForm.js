5210441dc555e667498f7e5a3fa83cc12c770f81
Mounika Marreddy <mouni@Mounika.local>
2024-06-24 10:01:39 +0530

    final

--- my-website/src/components/BookingForm.js
+++ my-website/src/components/BookingForm.js
@@ -29,21 +29,21 @@ function BookingForm({availableTimes, dispatch, submitForm}) {
          <div className='form-items-container'>
          <div className="form-items">
             <label htmlFor="res-date">Choose date:</label>
-            <input type="date" id="res-date" value={date} onChange={(e) => {
+            <input aria-label="Select date" type="date" id="res-date" value={date} onChange={(e) => {
                let selectedDate =  e.target.value
                setDate(selectedDate); 
                dispatch({type:"updateTimes", payload: selectedDate})
             }} required/>
             <label htmlFor="res-time">Choose time:</label>
-            <select id="res-time" value={time} onChange={(e) => setTime(e.target.value)}required>
+            <select aria-label="Select time" id="res-time" value={time} onChange={(e) => setTime(e.target.value)}required>
                {availableTimes.map((times, index) => 
                (<option key={index} value={times}>{times}</option>))
                } 
             </select>
             <label htmlFor="guests">Number of guests:</label>
-            <input type="number" placeholder="1" min="1" max="10" id="guests" value={guests} onChange={(e) => setGuests(e.target.value)}required/>
+            <input type="number" placeholder="1" aria-label="number of guests" min="1" max="10" id="guests" value={guests} onChange={(e) => setGuests(e.target.value)}required/>
         <label htmlFor="seating">Seating Preference</label>
-        <select id="seating" value={seating} onChange={(e)=> setSeating(e.target.value)}>
+        <select aria-label="seating preference" id="seating" value={seating} onChange={(e)=> setSeating(e.target.value)}>
                 <option value="Patio">Patio</option>
                 <option value="Bakery">Bakery</option>
                 <option value="Indoor">Indoor</option>
@@ -51,13 +51,13 @@ function BookingForm({availableTimes, dispatch, submitForm}) {
          </div>
          <div className="form-items">
         <label htmlFor="name">Name:</label>
-        <input type="text" placeholder='Name' id="name" value={name} onChange={(e) => setName(e.target.value)} required/>
+        <input type="text" placeholder='Name' aria-label="Your name" id="name" value={name} onChange={(e) => setName(e.target.value)} required/>
         <label htmlFor="email">Email:</label>
-        <input type="email" placeholder='email ID' id="email" value={email} onChange={(e) => setEmail(e.target.value)} required/>
+        <input type="email" placeholder='email ID' aria-label="Your email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} required/>
         <label htmlFor="phone">Phone Number:</label>
-        <input type="tel" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" placeholder='123-456-7890' id="phone" value={phone} onChange={(e) => setPhone(e.target.value)} required/>
+        <input type="tel" aria-label="Your phone number" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" placeholder='123-456-7890' id="phone" value={phone} onChange={(e) => setPhone(e.target.value)} required/>
         <label htmlFor="occasion">Occasion:</label>
-        <select id="occasion" value={occasion} onChange={(e)=> setOccasion(e.target.value)}>
+        <select aria-label="Select occasion" id="occasion" value={occasion} onChange={(e)=> setOccasion(e.target.value)}>
            <option>Birthday</option>
            <option>Anniversary</option>
            <option>None</option>
